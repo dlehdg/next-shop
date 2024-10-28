44109643161cf23c30b8dca3d06e42e2a5990bf4
dlehdg <ldg2409@naver.com>
2024-10-28 17:18:24 +0900

    [feat] footer 생성

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,36 +1,31 @@
 // Import the functions you need from the SDKs you need
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storage'
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
-
-
 const firebaseConfig = {
-  
-// API_KEY: process.env.NEXT_PUBLIC_API_KEY,
-// AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-// PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
-// STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-// MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-// APP_ID: process.env.NEXT_PUBLIC_APP_ID
+  // API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  // AUTH_DOMAIN: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  // PROJECT_ID: process.env.NEXT_PUBLIC_PROJECT_ID,
+  // STORAGE_BUCKET: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  // MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  // APP_ID: process.env.NEXT_PUBLIC_APP_ID
 
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID
-
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);

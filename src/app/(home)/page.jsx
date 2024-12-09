8a427661c8783c1935af11f92b7ac38491c7e066
dlehdg <ldg2409@naver.com>
2024-12-09 20:29:21 +0900

    [feat] sliderdata 추가 및 slider 컴포넌트 추가

--- src/app/(home)/page.jsx
+++ src/app/(home)/page.jsx
@@ -1,14 +1,16 @@
 import Header from "@/layouts/header/Header";
 import Image from "next/image";
 import Link from "next/link";
+import Slider from "./../../components/slider/Slider";
 
 export default function Home() {
   return (
-    <main>
+    <>
+      <Slider />
       <Header />
       <Link href="login">
         <h2>로그인 페이지로 이동</h2>
       </Link>
-    </main>
+    </>
   );
 }
